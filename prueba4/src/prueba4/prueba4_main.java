927e6d9181de45d5ce132c37dfa337da11abeb2b
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-09-29 21:16:05 +0200

    first commit

---
+++ prueba4/src/prueba4/prueba4_main.java
@@ -0,0 +1,10 @@
+package prueba4;
+
+public class prueba4_main {
+	public static void main(String args[]) {
+		for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+			System.out.println("FIN DEL PROGRAMA");
+	}
+
+}
